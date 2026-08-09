fcfccdc756cd369f0c06e066fedf165fe8500d4e
engus5416 <engus5416@gmail.com>
2026-08-09 21:42:32 +0900

    feat:패킷추가,어카운트서버연결

--- Assets/Scripts/Manager/LoginManager.cs
+++ Assets/Scripts/Manager/LoginManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class LoginManager : MonoBehaviour
@@ -20,35 +21,64 @@ public class LoginManager : MonoBehaviour
     }
 
     public bool TrySignUp(string id, string pw)
-    {        
+    {
         //회원가입 요청
-       
+
         //회원가입 성공
         //signupresult = (int)RQResult.Success;
 
         //회원가입 실패
-        //signupresult = (int)RQResult.Fail;
+        //signupresult = (int)RQResult.Fail;        
+        string account = id;
+        string password = pw;
+        if (string.IsNullOrWhiteSpace(account) || string.IsNullOrWhiteSpace(password))
+        {
+            return false;
+        }
 
-        return true;        
+        CreateAccountPacketReq packet = new CreateAccountPacketReq()
+        {
+            AccountName = account,
+            Password = password
+        };
+
+        WebManager.Instance.SendPostRequest<CreateAccountPacketRes>("account/create", packet, (res) =>
+        {
+            Debug.Log(res.CreateOk);            
+        });
+        return true;
     }
 
     public bool TryLogin(string id, string pw)
     {
         //서버에서 로그인 인증
-        //서버에서 유저 정보 받아옴
-        if (id=="test" && pw=="1234")
+        //서버에서 유저 정보 받아옴                    
+        
+        string account = id;
+        string password = pw;
+        if (string.IsNullOrWhiteSpace(account) || string.IsNullOrWhiteSpace(password))
         {
-            //success
-            usersession.UserLogin(id, 1); //서버에서 받아온 유저정보라고 가정           
-            
-            return true;
+            return false;
         }
-        else 
-        {
-           //fail
 
-           return false;
-        }           
+        LoginAccountPacketReq packet = new LoginAccountPacketReq()
+        {
+            AccountName = account,
+            Password = password
+        };
+        //웹서버 로그인 인증
+        WebManager.Instance.SendPostRequest<LoginAccountPacketRes>("account/login", packet, (res) =>
+        {
+            Debug.Log(res.LoginOk);            
+
+            if (res.LoginOk)
+            {
+                //success
+                usersession.UserLogin(id, 1); //서버에서 받아온 유저정보라고 가정  
+                NetworkManager.Instance.ConnectToGame(res.SessionKey, res.ServerInfo.Ip, res.ServerInfo.Port);                
+            }
+        });
+        return true;
     }
 
     public bool LogoutFromServer()
--- PacketGenerator/bin/Debug/ServerPacketManager.cs
+++ PacketGenerator/bin/Debug/ServerPacketManager.cs
@@ -23,6 +23,21 @@ public class PacketManager
         _makeFunc.Add((ushort)PacketID.C_RegLoginGameServer, MakePacket<C_RegLoginGameServer>);
         _handler.Add((ushort)PacketID.C_RegLoginGameServer, PacketHandler.C_RegLoginGameServerHandler);
 
+        _makeFunc.Add((ushort)PacketID.C_SendFriendReq, MakePacket<C_SendFriendReq>);
+        _handler.Add((ushort)PacketID.C_SendFriendReq, PacketHandler.C_SendFriendReqHandler);
+
+        _makeFunc.Add((ushort)PacketID.C_FriendReqRespons, MakePacket<C_FriendReqRespons>);
+        _handler.Add((ushort)PacketID.C_FriendReqRespons, PacketHandler.C_FriendReqResponsHandler);
+
+        _makeFunc.Add((ushort)PacketID.C_RemoveFriend, MakePacket<C_RemoveFriend>);
+        _handler.Add((ushort)PacketID.C_RemoveFriend, PacketHandler.C_RemoveFriendHandler);
+
+        _makeFunc.Add((ushort)PacketID.C_FriendList, MakePacket<C_FriendList>);
+        _handler.Add((ushort)PacketID.C_FriendList, PacketHandler.C_FriendListHandler);
+
+        _makeFunc.Add((ushort)PacketID.C_FriendReqList, MakePacket<C_FriendReqList>);
+        _handler.Add((ushort)PacketID.C_FriendReqList, PacketHandler.C_FriendReqListHandler);
+
     }
 
     public void OnRecvPacket(PacketSession session, ArraySegment<byte> buffer, Action<PacketSession, IPacket> onRecvCallback = null)
